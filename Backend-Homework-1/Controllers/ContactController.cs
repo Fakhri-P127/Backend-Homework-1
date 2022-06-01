c3d4003864539d7175ecfba1348600d31a6838db
Fakhri-P127 <fakhrita@code.edu.az>
2022-06-01 09:11:09 +0400

    added homework folder

---
+++ Backend-Homework-1/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Homework_1.Controllers
+{
+    public class ContactController:Controller
+    {
+        public ActionResult Form(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            //return Content(id.ToString());
+            return View();
+        }
+
+        public ContentResult Faq()
+        {
+            return Content("P127");
+        }
+    }
+}
---
+++ Backend-Homework-1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_Homework_1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            //ViewResult view = new ViewResult { ViewName = "HomePage" };
+            //return View();
+            return new ViewResult { ViewName = "HomePage" };
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
